c90817804f4e0427191028000b698b51b6ad3921
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2023-02-03 07:32:41 -0600

    classifier model building

--- main_script.py
+++ main_script.py
@@ -1,12 +1,35 @@
 # Import the required libraries
+import os
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 
 # Load the data
-text_data = [...] # Replace with the text data
-labels = [...] # Replace with the corresponding labels (0 or 1)
+root_folder = 'data'
+
+text_data = [] # Replace with the text data
+labels = [] # Replace with the corresponding labels (0 or 1)
+
+for subfolder in os.listdir(root_folder):
+    subfolder_path = os.path.join(root_folder, subfolder)
+
+    if subfolder == 'fitzgerald':
+        label = 0
+    else:
+        label = 1
+
+    for file in os.listdir(subfolder_path):
+        file_path = os.path.join(subfolder_path, file)
+        print("Processing file:", file)
+        with open(file_path, 'r') as f:
+            text = f.read()
+
+        text_data.append(text)
+        labels.append(label)
+
+#print(len(text_data))
+#print(len(labels))
 
 # Split the data into training and testing sets
 text_train, text_test, labels_train, labels_test = train_test_split(text_data, labels, test_size=0.2)
@@ -22,7 +45,27 @@ classifier.fit(X_train, labels_train)
 
 # Predict the labels on the test data
 predictions = classifier.predict(X_test)
+predictions = [1 if prediction > 0.5 else 0 for prediction in predictions]
+accuracy = accuracy_score(labels_test, predictions)
 
 # Evaluate the classifier
 accuracy = accuracy_score(labels_test, predictions)
+print("Accuracy:", accuracy)
+
+for i in range(len(X_test)):
+    print("Text:", X_test[i][:100], "...")
+    print("Actual label:", labels_test[i])
+    print("Predicted label:", predictions[i])
+
+# Convert the sparse matrix to a dense matrix
+X_test = X_test.toarray()
+
+# Make predictions on the test data
+predictions = model.predict(X_test)
+
+# Convert the predictions from numeric to binary format
+predictions = [1 if prediction > 0.5 else 0 for prediction in predictions]
+
+# Calculate the accuracy of the classifier
+accuracy = accuracy_score(y_test, predictions)
 print("Accuracy:", accuracy)
--- strip_headers.py
+++ strip_headers.py
@@ -1,5 +1,40 @@
 import os
 
 directory = 'data'
-
+'''
+for root, dirs, files in os.walk(folder_path):
+    for filename in files:
+        if filename.endswith('.txt'):
+            file_path = os.path.join(root, filename)
+            try:
+                with open(file_path, "rb") as file:
+                    data = file.read()
+        # process data
+            except UnicodeDecodeError as e:
+                print(f"Error processing file {filename}: {e}")
+'''
 for root, dirs, files in os.walk(directory):
+    for filename in files:
+        if filename.endswith('.txt'):
+            file_path = os.path.join(root, filename)
+
+            with open(file_path, "r", encoding='ISO-8859-1') as file:
+                lines = file.readlines()
+            start = "*** START OF THE PROJECT GUTENBERG"
+            end = "*** END OF THE PROJECT GUTENBERG"
+            start_indices = [i for i, line in enumerate(lines) if start in line]
+            if not start_indices:
+                continue
+            start_index = start_indices[0] + 1
+            try:
+                end_index = lines.index(end)
+            except ValueError:
+                continue
+            stripped_lines = lines[start_index:end_index]
+            stripped_file_path = os.path.join(root, 'stripped_' + filename)
+            with open(stripped_file_path, 'w') as stripped_file:
+                stripped_file.writelines(stripped_lines)
+
+
+
+
