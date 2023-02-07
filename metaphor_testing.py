f4bb34d40c212ef4eefa5cc0d31996ed7e033189
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2023-02-07 06:23:21 -0600

    updates

--- metaphor_testing.py
+++ metaphor_testing.py
@@ -1,3 +1,4 @@
+'''
 from nltk import word_tokenize
 from nltk.util import ngrams
 import nltk
@@ -38,3 +39,33 @@ def extract_metaphors_similes(text):
 test_paragraph = "The classroom was a zoo. The alligator's teeth are white daggers. She was such a peacock, strutting around with her colorful new hat. My teacher is a dragon ready to scold anyone he looks at. Mary's eyes were fireflies. The computers at school are old dinosaurs. He is a night owl. Maria is a chicken. The wind was a howling wolf. The ballerina was a swan, gliding across the stage. Jamal was a pig at dinner. The kids were monkeys on the jungle gym. My dad is a road hog. The stormy ocean was a raging bull. The thunder was a mighty lion. In this summer heat, the kids were just a bunch of lazy dogs. The snow was a white blanket over the sleepy town. He is a shining star on that stage. Her fingers were icicles after playing outside. Her long hair was a flowing golden river. The children were flowers grown in concrete gardens. The falling snowflakes are dancers. The calm lake was a mirror. You are my sunshine. The moon was a white balloon floating over the city. The road ahead was a ribbon stretching across the desert. The park was a lake after the rain. The sun is a golden ball. The clouds are balls of cotton. The lightning was fireworks in the sky. That lawn was a perfect green carpet after getting mowed this morning. The stars are sparkling diamonds. Ben's temper was a volcano ready to explode. Those best friends are two peas in a pod."
 sentence_1_count = count_metaphors_nltk(test_sentences[1])
 print(sentence_1_count)
+'''
+
+import json
+import matplotlib.pyplot as plt
+
+with open("results.json") as f:
+    data = json.load(f)
+
+hemingway_data = [(k.split("/")[2], v) for k, v in data.items() if k.startswith("data/hemingway/")]
+fitzgerald_data = [(k.split("/")[2], v) for k, v in data.items() if k.startswith("data/fitzgerald/")]
+
+hemingway_names, hemingway_counts = zip(*hemingway_data)
+fitzgerald_names, fitzgerald_counts = zip(*fitzgerald_data)
+print(hemingway_names, hemingway_counts)
+
+
+# create bar plot
+bar_width = 0.35
+
+fig, ax = plt.subplots()
+
+rects1 = ax.bar(fitzgerald_names, fitzgerald_counts, bar_width, label='Fitzgerald')
+rects2 = ax.bar(hemingway_names, hemingway_counts, bar_width, label='Hemingway')
+
+ax.set_xlabel('Book')
+ax.set_ylabel('Count')
+ax.legend()
+plt.xticks(rotation=90)
+plt.show()
+plt.savefig('metaphor_counts.png')
